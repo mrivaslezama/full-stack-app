e8b2a701e107dc83a7341c48203f2bac003ca3d7
Miguel Rivas <mrivaslezma@gmail.com>
2024-07-21 14:46:28 -0400

    dinamic changes

--- full_stack_python/full_stack_python.py
+++ full_stack_python/full_stack_python.py
@@ -7,8 +7,18 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
+    label = "Somos una empresa familar con más de 10 años de experiencia en el mercado."
+    orginal_label = label
+    cool_label = "Queremos ayudarte a desarrollar tus proyectos"
 
+    def change_label(self):
+        if self.label == "Queremos ayudarte a desarrollar tus proyectos":
+            self.label = self.orginal_label
+        else:
+            self.label = "Queremos ayudarte a desarrollar tus proyectos"
     ...
+
+
 
 
 def index() -> rx.Component:
@@ -16,12 +26,16 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading("Bienvenidos!", size="9"),
+             rx.heading("Somos Autana Muebles", size="7"),
+              rx.heading(State.label, " Diseño y construcción de muebles", size="5"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
                 size="5",
             ),
+            # Dinamic changes - event handler on_click
+            rx.button("Do something!", on_click=State.change_label),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
